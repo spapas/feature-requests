dacca31328dd56fe39df05a34282a26ad2abda77
Serafeim Papastefanos <spapas@gmail.com>
2018-11-08 14:26:55 +0200

    Improve form and fornmhelpers

--- core/forms.py
+++ core/forms.py
@@ -2,5 +2,11 @@ from wtforms import Form, StringField, TextField, validators
 
 
 class FeatureRequestForm(Form):
-    title = StringField("Title", [validators.Required()])
-    description = TextField("Description", [validators.Required()])
+    title = StringField(
+        "Title", description="Feature request title", validators=[validators.Required()]
+    )
+    description = TextField(
+        "Description",
+        description="Feature request description",
+        validators=[validators.Required()],
+    )
