5490da3d6c5f8dbbcadd91e54fed635e28843e63
Serafeim Papastefanos <spapas@gmail.com>
2018-11-08 21:23:28 +0200

    Add instance conf

--- core/__init__.py
+++ core/__init__.py
@@ -3,9 +3,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('core.default_settings')
-app.config.from_envvar('LOCAL_SETTINGS')
+app.config.from_pyfile('local.py')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
--- rundev.bat
+++ rundev.bat
@@ -1,6 +1,5 @@
 set WERKZEUG_DEBUG_PIN=off
 set FLASK_APP=core
-set LOCAL_SETTINGS=local_settings.py
 set FLASK_ENV=debug
 
 flask run
