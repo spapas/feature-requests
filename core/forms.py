667d0072363bc7098cd58764c72c3c6adcaf69a8
Serafeim Papastefanos <spapas@gmail.com>
2018-11-08 14:37:56 +0200

    Add a select field

--- core/forms.py
+++ core/forms.py
@@ -1,12 +1,19 @@
-from wtforms import Form, StringField, TextField, validators
+from wtforms import Form, StringField, TextAreaField, validators, SelectField
 
 
 class FeatureRequestForm(Form):
     title = StringField(
         "Title", description="Feature request title", validators=[validators.Required()]
     )
-    description = TextField(
+    description = TextAreaField(
         "Description",
         description="Feature request description",
         validators=[validators.Required()],
     )
+    client = SelectField(
+        u"Client", description="Select the client for this feature request"
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.client.choices=( ('A', 'b'), ('c', 'd'),  )
