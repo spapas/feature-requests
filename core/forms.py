bb724a8b629cf43c841ce4ea21b045026ed71fd8
Serafeim Papastefanos <spapas@gmail.com>
2018-11-08 14:45:06 +0200

    Finalize form

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,12 @@
-from wtforms import Form, StringField, TextAreaField, validators, SelectField
+from wtforms import (
+    Form,
+    StringField,
+    TextAreaField,
+    validators,
+    SelectField,
+    IntegerField,
+    DateField,
+)
 
 
 class FeatureRequestForm(Form):
@@ -13,7 +21,17 @@ class FeatureRequestForm(Form):
     client = SelectField(
         u"Client", description="Select the client for this feature request"
     )
+    client_priority = IntegerField(
+        u"Client priority", description="Enter the priority for the feature request."
+    )
+    target_date = DateField(
+        u"Target date", description="Select the target date for this feature request using the format 'YYYY-MM-DD'"
+    )
+    product_area = SelectField(
+        u"Product area", description="Select the product area for this feature request"
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.client.choices=( ('A', 'b'), ('c', 'd'),  )
+        self.client.choices = (("A", "b"), ("c", "d"))
+        self.product_area.choices = (("A", "b"), ("c", "d"))
