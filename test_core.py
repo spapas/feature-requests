7958213a7f2df293a0b277c5b7cb690980f5e05d
Serafeim Papastefanos <spapas@gmail.com>
2018-11-09 10:16:57 +0200

    Add create feature req test

--- test_core.py
+++ test_core.py
@@ -2,7 +2,7 @@ from flask_testing import TestCase
 from flask import url_for
 from core import app, db
 import unittest
-from core.models import FeatureRequest
+from core.models import FeatureRequest, Client, ProductArea
 import datetime
 
 
@@ -31,7 +31,7 @@ class HomepageTest(BaseTest):
 
 
 class ListpageTest(BaseTest):
-    def test_3_add_more_feature_requests(self):
+    def test_listpage(self):
         response = self.client.get(url_for("feature_requests_view"))
         assert b"No feature requests found." in response.data
         response = self.client.get(url_for("feature_requests_view"))
@@ -65,6 +65,58 @@ class ListpageTest(BaseTest):
             url_for("feature_requests_delete", feature_request_id=1).encode()
             in response.data
         )
+
+
+class CreatepageTest(BaseTest):
+    def test_createpage(self):
+        response = self.client.get(url_for("feature_requests_create"))
+        assert b"Add Feature Request" in response.data
+        assert b"<form method='POST'>" in response.data
+        assert b"form-group has-error" not in response.data
+
+        response = self.client.post(url_for("feature_requests_create"), data=dict(
+            title="Title",
+            description="Desc",
+            client=None,
+            client_priority=1,
+            target_date=datetime.date(2018, 1, 1),
+            product_area=None,
+        ))
+
+        assert b"form-group has-error" in response.data
+        assert b"<form method='POST'>" in response.data
+        assert response.status == '200 OK'
+
+        client = Client("C1")
+        db.session.add(client)
+        product_area = ProductArea("PA1")
+        db.session.add(product_area)
+        db.session.commit()
+        response = self.client.post(url_for("feature_requests_create"), data=dict(
+            title="Title",
+            description="Desc",
+            client=client.id,
+            client_priority=1,
+            target_date=datetime.date(2018, 1, 1),
+            product_area=product_area.id,
+        ))
+        assert response.status == '302 FOUND'
+
+        response = self.client.post(url_for("feature_requests_create"), data=dict(
+            title="Title",
+            description="Desc",
+            client=client.id,
+            client_priority=1,
+            target_date=datetime.date(2018, 1, 1),
+            product_area=product_area.id,
+        ), follow_redirects=True)
+        assert response.status == '200 OK'
+        assert b"Feature request created!" in response.data
+        assert response.data.count(b'Update') == 2
+        assert response.data.count(b'Delete') == 2
+        assert client.name.encode() in response.data
+        assert product_area.name.encode() in response.data
+
 
 if __name__ == "__main__":
     unittest.main()
